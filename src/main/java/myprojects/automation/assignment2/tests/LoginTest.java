74ca10fae459ed9e20b52e08a287b55baf1043d6
Aconic <tygreman@gmail.com>
2017-04-10 17:27:14 +0300

    Explicit driver wait added

--- src/main/java/myprojects/automation/assignment2/tests/LoginTest.java
+++ src/main/java/myprojects/automation/assignment2/tests/LoginTest.java
@@ -4,6 +4,7 @@ import myprojects.automation.assignment2.BaseScript;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static myprojects.automation.assignment2.utils.Properties.getBaseAdminUrl;
@@ -15,7 +16,7 @@ public class LoginTest extends BaseScript {
 
         WebDriver driver = getDriver();
         driver.get(getBaseAdminUrl());
-        WebDriverWait wait= new WebDriverWait(driver, 5);
+;
         WebElement email= driver.findElement(By.id("email"));
         WebElement passwd = driver.findElement(By.id("passwd"));
         WebElement submit = driver.findElement(By.name("submitLogin"));
@@ -24,14 +25,14 @@ public class LoginTest extends BaseScript {
         passwd.sendKeys("Xcg7299bnSmMuRLp9ITw");
         submit.click();
 
-        Thread.sleep(2000);
+        (new WebDriverWait(driver, 4))
+                .until(ExpectedConditions.presenceOfElementLocated(By.className("img-thumbnail")));
+        
         WebElement user = driver.findElement(By.className("img-thumbnail"));
         user.click();
 
         WebElement logout = driver.findElement(By.id("header_logout"));
         logout.click();
-
-        Thread.sleep(5000);
         driver.quit();
     }
 }
